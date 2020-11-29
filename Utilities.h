966d4f74b08b9be62846f2b72d1ca127c0b56899
Kritor-cyber <thibault.denier@efrei.net>
2020-11-29 11:22:33 +0100

    Désormais le fichier est écrit en faux binaire et en vrai binaire (le vrai binaire est identique à l'originale)

--- Utilities.h
+++ Utilities.h
@@ -7,3 +7,5 @@ void PrintErrorMessageFromErrorCodeFromFile(errno_t err);
 char* AddStringBeforeExtensionOfFileName(char* fileName, char* stringToAdd);
 
 int GetNumberCharInFile(char* path);
+
+void WriteInBinary(FILE* file, char* string);
